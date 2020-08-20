99e4b0bf8eac8db2d136b01f3c5fe1f7edf7420e
Vital Ozierski <v.ozierski@ozitag.com>
2020-08-20 23:34:10 +0300

    Fix Typo

--- src/Fields/MultiSelectField.php
+++ src/Fields/MultiSelectField.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace OZiTAG\Tager\Backend\Fields\Structures;
+namespace OZiTAG\Tager\Backend\Fields\Fields;
 
 use OZiTAG\Tager\Backend\Fields\Base\Field;
 use OZiTAG\Tager\Backend\Fields\Enums\FieldType;
