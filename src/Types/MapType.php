0a23449d69a9ab5c364b68dff8261579093835a5
Vital Ozierski <v.ozierski@ozitag.com>
2020-09-23 00:36:10 +0300

    map type improve

--- src/Types/MapType.php
+++ src/Types/MapType.php
@@ -11,4 +11,59 @@ class MapType extends StringType
     {
         return FieldType::Map;
     }
+
+    public function getDatabaseValue()
+    {
+        if (is_array($this->value) && isset($this->value['latitude']) && isset($this->value['longitude'])) {
+            return floatval($this->value['latitude']) . ';' . floatval($this->value['longitude']);
+        } else {
+            return null;
+        }
+    }
+
+    public function loadValueFromDatabase($value)
+    {
+        if (!$value) {
+            $this->setValue(null);
+            return;
+        }
+
+        $value = explode(';', $value);
+        if (count($value) !== 2) {
+            $this->setValue(null);
+            return;
+        }
+
+        $latitude = $value[0];
+        $longitude = $value[1];
+
+        if (!is_numeric($latitude) || !is_numeric($longitude)) {
+            $this->setValue(null);
+            return;
+        }
+
+        $this->setValue([
+            'latitude' => floatval($latitude),
+            'longitude' => floatval($longitude)
+        ]);
+    }
+
+    public function getPublicValue()
+    {
+        return $this->getAdminFullJson();
+    }
+
+    public function getAdminJson()
+    {
+        return $this->getAdminFullJson();
+    }
+
+    public function getAdminFullJson()
+    {
+        if (!$this->value) {
+            return null;
+        }
+
+        return $this->value;
+    }
 }
