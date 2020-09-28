8fd2e2bc74ee06c598251f6f942420172c026c77
Vital Ozierski <v.ozierski@ozitag.com>
2020-09-28 16:29:29 +0300

    Repeater - public value

--- src/Types/RepeaterType.php
+++ src/Types/RepeaterType.php
@@ -6,6 +6,8 @@ use Doctrine\DBAL\Types\TextType;
 use OZiTAG\Tager\Backend\Fields\Base\Field;
 use OZiTAG\Tager\Backend\Fields\Base\Type;
 use OZiTAG\Tager\Backend\Fields\Enums\FieldType;
+use OZiTAG\Tager\Backend\Fields\Fields\RepeaterField;
+use OZiTAG\Tager\Backend\Fields\TypeFactory;
 
 class RepeaterType extends Type
 {
@@ -45,6 +47,41 @@ class RepeaterType extends Type
         if ($value) {
             $this->setValue(json_decode($value, true));
         }
+    }
+
+    public function getPublicValue()
+    {
+        $value = $this->getValue();
+        $fields = $this->fields;
+
+        $result = [];
+
+        foreach ($value as $valueRow) {
+            $resultRow = [];
+
+            foreach ($fields as $field) {
+
+                $fieldValue = null;
+                foreach ($valueRow as $valueItem) {
+                    if ($valueItem['name'] == $field->getName()) {
+                        $fieldValue = $valueItem['value'];
+                        break;
+                    }
+                }
+
+                $type = $field->getTypeInstance();
+                if ($field instanceof RepeaterField && $type instanceof RepeaterType) {
+                    $type->setFields($field->getFields());
+                }
+                $type->setValue($fieldValue);
+
+                $resultRow[$field->getName()] = $type->getPublicValue();
+            }
+
+            $result[] = $resultRow;
+        }
+
+        return $result;
     }
 
     public function setValue($value)
